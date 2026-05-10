ff1d3836dfb6e319a3cb146c469a3af3d1c45649
Martha Meslina Florencia <mmeslinafs@gmail.com>
2026-05-11 02:10:49 +0700

    fix the UI and add the logic for animation

---
+++ Assets/Scripts/FreeplayData.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class HanziRecipe
+{
+    public string pinyin; 
+    public List<string> requiredCards; 
+    
+    [Header("Visual Result")]
+    [Tooltip("Tarik Prefab INDUK (kotak biru) yang berisi semua line ke sini")]
+    public GameObject hanziPrefab; 
+}
+
+[CreateAssetMenu(fileName = "NewFreeplayData", menuName = "Zichara/Freeplay Data")]
+public class FreeplayData : ScriptableObject
+{
+    public string dataID;
+    public List<HanziRecipe> hanziRecipes;
+}
